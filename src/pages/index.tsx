1e7aadb4f2a731c74cbf2983772271463bb93e4b
Pedro Igor <picoliveira92@gmail.com>
2021-04-25 09:42:11 -0300

    End of day 2. Fake API json-server now being used

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1>Index</h1>;
+// SPA (Single Page Application)
+// useEffect(() => {
+//   fetch("http://localhost:3333/episodes")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// }, []);
+// SSR (Server Side Rendering) same as current, but with getServerSiderProps()
+// SSG (Static Site Generation) only works in production
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
